2a48fef38070854a122eb2b065ad8809c164ec71
Brett <40072395+Planchet76@users.noreply.github.com>
2019-01-03 16:39:14 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,43 @@
+function mainFunction01(){
+	fetch('http://restapi.demoqa.com/utilities/weather/city/Dallas')
+	.then(
+		function(response) {
+			if (response.status !== 200) {
+				console.log('Looks like there was a problem. Status Code: ' +
+				response.status);
+				return;
+			}
+
+			response.json().then(function(data) {
+				console.log(data);
+				document.getElementById("id_results").innerHTML = JSON.stringify(data);
+			});
+		}
+	)
+	.catch(function(err) {
+		console.log('Fetch Error :-S', err);
+	});
+}
+
+function mainFunction02() {
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+				if (this.readyState == 4) {
+						document.getElementById("id_results").innerHTML = this.responseText;
+				}
+		};
+		xhttp.open("GET", "https://pokeapi.co/api/v2/berry/", true);
+		xhttp.send();
+}
+
+function tstFunction01(){
+	document.getElementById("id_results").innerHTML="No soup for you.";
+}
+
+function tstFunction02(){
+	document.getElementById("id_results").innerHTML="Are you deaf?";
+}
+
+function tstFunction03(){
+	document.getElementById("id_results").innerHTML="I SAID NO SOUP!";
+}
